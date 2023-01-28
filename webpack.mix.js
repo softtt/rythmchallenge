113a1e9c71ed327ccb17b3755c92f77e99eae00a
softtt <vitaliy.sheynin@mail.ru>
2023-01-28 18:06:18 +0300

    add auth and fix npm run dev bug

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .postCss('resources/css/app.css', 'public/css', [require('tailwindcss'), require('autoprefixer')])
+    .alias({
+        '@': 'resources/js',
+    }).vue();
+
+if (mix.inProduction()) {
+    mix.version();
+}
